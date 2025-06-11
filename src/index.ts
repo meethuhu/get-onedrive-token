dbfbde453fd9ccb122f54cd692506d5a44870be7
meethuhu <meethuhu@gmail.com>
2025-06-12 00:42:56 +0800

    修复手机端显示问题

--- src/index.ts
+++ src/index.ts
@@ -277,7 +277,8 @@ async function handleCallback(request: Request): Promise<Response> {
 		<html>
 		<head>
 			<title>完成授权</title>
-			<meta charset="UTF-8">
+    		<meta charset="UTF-8">
+    		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 			<style>
 				body { 
 					font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; 
